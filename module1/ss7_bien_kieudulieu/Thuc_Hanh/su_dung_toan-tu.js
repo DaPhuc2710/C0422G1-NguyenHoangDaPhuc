29531ac00bb2a84fcda9fb95cc1318cd83ce99af
DaPhuc2710 <daphuc2000@gmail.com>
2022-05-10 17:14:08 +0700

    hehehehe

---
+++ module1/ss7_bien_kieudulieu/Thuc_Hanh/su_dung_toan-tu.js
@@ -0,0 +1,6 @@
+let inputwidth=prompt("Enterwidth");
+let inputheight=prompt("Enterheight");
+let width=parseInt(inputwidth);
+let height=parseInt(inputheight);
+let area=width*height;
+document.write("the area is"+area)
